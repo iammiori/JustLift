7a5e7befd49ce4536e9206e57427445aa0e8dd83
Mi Yeon Lee <immioriii@gmail.com>
2022-09-22 23:50:18 +0900

    feat: 메인이 될 리스트뷰 생성

---
+++ JustLift/LiftList/LiftListView.swift
@@ -0,0 +1,20 @@
+//
+//  LiftListView.swift
+//  JustLift
+//
+//  Created by miori Lee on 2022/09/22.
+//
+
+import SwiftUI
+
+struct LiftListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LiftListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LiftListView()
+    }
+}
