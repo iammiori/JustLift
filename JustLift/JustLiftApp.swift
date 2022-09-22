67397e1438d6e0681b5083c242d9abb2bc051ad9
Mi Yeon Lee <immioriii@gmail.com>
2022-09-22 21:00:45 +0900

    feat: 프로젝트 생성

---
+++ JustLift/JustLiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustLiftApp.swift
+//  JustLift
+//
+//  Created by miori Lee on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct JustLiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
