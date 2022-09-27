d36ca062ec7da66978a9666d2cd02ea230c9af83
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-09-27 23:29:09 +0900

    feat: 훈련 내용 상세보기가 가능한 뷰 구현 (#9)

---
+++ JustLift/LiftDetail/View/LiftDetailView.swift
@@ -0,0 +1,59 @@
+//
+//  LiftDetailView.swift
+//  JustLift
+//
+//  Created by miori Lee on 2022/09/27.
+//
+
+import SwiftUI
+
+struct LiftDetailView: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    var liftLog: LiftLog
+    let viewModel: LiftDetailViewModel = LiftDetailViewModel()
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack(spacing: 50) {
+                    Text(viewModel.formattedDate(dateString: liftLog.date))
+                        .font(.system(size: 30, weight: .bold))
+                    
+                    HStack {
+                        HStack {
+                            Text(liftLog.condition.emoji)
+                                .font(.system(size: 50))
+                            IntensityListCell(liftLog: liftLog)
+                                .frame(height: 60)
+                        }
+                    }
+                    
+                    Text(liftLog.textLog)
+                        .font(.system(size: 20, weight: .regular))
+                }
+                .foregroundColor(colorScheme == .dark ? .white : .black)
+            }
+            
+            
+            HStack {
+                Button {
+                    print("delete tapped")
+                } label: {
+                    Image(systemName: "trash")
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20)
+                }
+            }
+        }
+    }
+}
+
+struct LiftDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LiftDetailView(liftLog: LiftLog.list.first!)
+    }
+}
