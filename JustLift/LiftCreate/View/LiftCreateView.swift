2c8d60158b2e9748365173e0d30ba7f22f306d24
Mi Yeon Lee <immioriii@gmail.com>
2022-09-30 22:22:39 +0900

    feat: 운동컨디션과 난이도를 선택할수 있는 뷰로 이동

--- JustLift/LiftCreate/View/LiftCreateView.swift
+++ JustLift/LiftCreate/View/LiftCreateView.swift
@@ -10,19 +10,28 @@ import SwiftUI
 struct LiftCreateView: View {
     
     @StateObject var viewModel: LiftCreateViewModel
+    @Binding var isPresented: Bool
     
     var body: some View {
-        VStack {
-            DatePicker("start date", selection: $viewModel.date, displayedComponents: [.date])
-                .datePickerStyle(.graphical)
-            Spacer()
+        NavigationView {
+            VStack {
+                DatePicker("start date", selection: $viewModel.date, displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                Spacer()
+                NavigationLink {
+                    InAndConInputIvew(vm: viewModel)
+                } label: {
+                    Text("Next")
+                }
+
+            }
         }
     }
 }
 
 struct LiftCreateView_Previews: PreviewProvider {
     static var previews: some View {
-        let vm = LiftCreateViewModel(isPresented: .constant(false))
-        LiftCreateView(viewModel: vm)
+        let vm = LiftCreateViewModel()
+        LiftCreateView(viewModel: vm, isPresented: .constant(false))
     }
 }
