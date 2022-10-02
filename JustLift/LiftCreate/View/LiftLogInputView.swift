43b68b8d3c7b6f62bd59e7548fbdd4b6dbe2c58e
Mi Yeon Lee <immioriii@gmail.com>
2022-10-02 22:09:54 +0900

    feat: 훈련내용을 기록할수 있는 뷰 생성

---
+++ JustLift/LiftCreate/View/LiftLogInputView.swift
@@ -0,0 +1,20 @@
+//
+//  LiftLogInputView.swift
+//  JustLift
+//
+//  Created by miori Lee on 2022/10/01.
+//
+
+import SwiftUI
+
+struct LiftLogInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LiftLogInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        LiftLogInputView()
+    }
+}
