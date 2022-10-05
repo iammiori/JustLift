921bdd669f4258e2f9331cd58ea0a77ee59eb298
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-10-05 23:26:14 +0900

    feat: onAppear시 focus를 통해 키보드 자동으로 올라오게 설정 (#20)

--- JustLift/LiftCreate/View/LiftLogInputView.swift
+++ JustLift/LiftCreate/View/LiftLogInputView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct LiftLogInputView: View {
     
     @ObservedObject var vm: LiftCreateViewModel
+    @FocusState var isfocused: Bool
     
     var body: some View {
         VStack {
             TextEditor(text: $vm.logText)
+                .focused($isfocused)
                 .border(.blue.opacity(0.2), width: 2)
         }
         .padding()
+        .onAppear {
+            isfocused = true
+        }
     }
 }
 
