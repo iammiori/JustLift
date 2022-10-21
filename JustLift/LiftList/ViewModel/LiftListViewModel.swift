227eb278ea97b0950d97831dce27269552c6583a
Mi Yeon Lee <immioriii@gmail.com>
2022-10-21 22:10:56 +0900

    feat: Storage 저장을 위한 함수 구현 (bind, persist)

--- JustLift/LiftList/ViewModel/LiftListViewModel.swift
+++ JustLift/LiftList/ViewModel/LiftListViewModel.swift
@@ -6,15 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 final class LiftListViewModel: ObservableObject {
+    
+    let storage: LiftlogStorage
+    
     //MARK: TO Do : 월 구분
-    @Published var liftList: [LiftLog] = LiftLog.list
+    //LiftLog.list
+    @Published var liftList: [LiftLog] = []
     @Published var listDic: [String: [LiftLog]] = [:]
     @Published var naviTitle: String = "역도왕의 길"
     
-    init() {
-        self.listDic = Dictionary(grouping: self.liftList, by: { $0.monthlyIdentifier })
+    var subscriptions = Set<AnyCancellable>()
+    
+    init(storage: LiftlogStorage) {
+        self.storage = storage
+        //self.listDic = Dictionary(grouping: self.liftList, by: { $0.monthlyIdentifier })
+        bind()
     }
     
     var keys: [String] {
@@ -42,4 +51,24 @@ final class LiftListViewModel: ObservableObject {
         formatter.dateFormat = "MMMM yyyy"
         return formatter.string(from: date)
     }
+    
+    func fetch() {
+        self.liftList = storage.fetch()
+    }
+    
+    private func bind() {
+        $liftList.sink { logs in
+            self.listDic = Dictionary(grouping: logs, by: { $0.monthlyIdentifier })
+            self.persist(logs: logs)
+        }.store(in: &subscriptions)
+    }
+    
+    private func persist(logs: [LiftLog]) {
+        guard logs.isEmpty == false else { return }
+        self.storage.persist(logs)
+    }
 }
+
+// 데이터 파일에서 훈련 리스트 가져오기
+// list에 해당 객체 세팅
+// list 세팅 되면 dic도
