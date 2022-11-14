4b5b5590f70024e9a5a869404194493ec234d7e6
Mi Yeon Lee <immioriii@gmail.com>
2022-11-14 23:39:32 +0900

    test: liftlist뷰모델 테스트 코드 추가

---
+++ JustLiftTests/LiftListVMTest.swift
@@ -0,0 +1,18 @@
+//
+//  LiftListVMTest.swift
+//  JustLiftTests
+//
+//  Created by miori Lee on 2022/11/14.
+//
+
+import XCTest
+@testable import JustLift
+
+final class LiftListVMTest: XCTestCase {
+    
+    var sut: LiftListViewModel!
+    var model: LiftLog!
+    
+
+    
+}
